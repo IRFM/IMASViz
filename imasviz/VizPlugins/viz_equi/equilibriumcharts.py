09e05e0ca06debe5032d71046ed93336f24bedc5
Dejan Penko <dejan.penko@lecad.fs.uni-lj.si>
2019-01-14 11:20:47 +0100

    Equilibriumcharts plugin: Fix and improve plugin test scripts.

--- imasviz/VizPlugins/viz_equi/equilibriumcharts.py
+++ imasviz/VizPlugins/viz_equi/equilibriumcharts.py
@@ -57,9 +57,9 @@ def DataGen(dictDataSource, dataTreeView):
     #run = 0
     #machine = 'west'
     #user = getpass.getuser()
-    dataSource = dictDataSource['imasviz_view'].dataSource
     if dataTreeView == None:
         dataTreeView = dictDataSource['imasviz_view']
+    dataSource = dataTreeView.dataSource
     shot = dataSource.shotNumber
     run = dataSource.runNumber
     machine = dataSource.imasDbName
@@ -73,10 +73,13 @@ def DataGen(dictDataSource, dataTreeView):
 
     # Open shot and run of machine
     occurrence = 0 # default occurrence
-    idd = dataSource.ids[occurrence]
+    try:
+        idd = dataSource.ids[occurrence]
+    except:
+        idd = None
     if idd is None:
-        dataSource.load(self.dataTreeView, occurrence=0, pathsList=None,
-                        async=False)
+        dataSource.load(dataTreeView, IDSName='equilibrium', occurrence=0,
+                        pathsList=None, async=False)
         idd = dataSource.ids[occurrence]
 
     if not dataTreeView.idsAlreadyFetched["equilibrium"]:
@@ -1014,37 +1017,56 @@ class equilibriumcharts(VizPlugins):
 
 
 if (__name__ == '__main__'):
-    #print('')
-    #print('For help about this function, type:')
-    #print('python equilibriumcharts.sh -h')
-    #print('')
+    # Test running. See also equilibrium test file.
 
-    # Parse input arguments
-    parser = argparse.ArgumentParser(description= \
-            'GUI for exploration of WEST equilibrium reconstruction calculated with VACTH-EQUINOX')
-    parser.add_argument('shot', type=int, nargs='?', default=52203, \
-                        help='shot, default=52203')
-    parser.add_argument('run', type=int, nargs='?', default=0, \
-                        help='run, default=0')
-    parser.add_argument('user', type=str, nargs='?', default='imas_public', \
-                        help='user, default=imas_public')
-    parser.add_argument('machine', type=str, nargs='?', default='west', \
-                        help='machine, default=west')
-    #parser.add_argument('--fast', action='store_true', \
-    #                    help='fast calculation')
+    # A module providing a number of functions and variables that can be used to
+    # manipulate different parts of the Python runtime environment.
+    import sys
+    # PyQt library imports
+    from PyQt5.QtWidgets import QApplication
+    # IMASViz source imports
+    from imasviz.Viz_API import Viz_API
+    from imasviz.VizDataSource.QVizDataSourceFactory import \
+        QVizDataSourceFactory
+    from imasviz.VizUtils.QVizGlobalOperations import QVizGlobalOperations
+    from imasviz.VizGUI.VizGUICommands.VizMenusManagement.QVizSignalHandling \
+        import QVizSignalHandling
 
-    args = parser.parse_args()
-
-    dictDataSource = {'shot': args.shot,
-                      'run': args.run,
-                      'user': args.user,
-                      'machine': args.machine}
-    #importedClass = my_import('equilibriumcharts')
-    #pluginEquiPlot = importedClass()
-    #pluginEquiPlot.execute(dictDataSource)
-
-    # Launch GUI
+    # Set object managing the PyQt GUI application's control flow and main
+    # settings
     app = QApplication(sys.argv)
-    app.frame = PlotFrame(dictDataSource)
+
+    # Check if necessary system variables are set
+    QVizGlobalOperations.checkEnvSettings()
+
+    # Set Application Program Interface
+    api = Viz_API()
+
+    # Set data source retriever/factory
+    dataSourceFactory = QVizDataSourceFactory()
+
+    # Load IMAS database and build the data tree view frame
+    f1 = api.CreateDataTree(dataSourceFactory.create(shotNumber=52344,
+                                                     runNumber=0,
+                                                     userName='g2penkod',
+                                                     imasDbName='viztest'))
+
+    # Get equilibrium treeWidget item (QVizTreeNode)
+    eq_item = f1.dataTreeView.IDSRoots['equilibrium']
+
+    # Get selected item/subject data dict
+    dataDict = eq_item.getDataDict()
+
+    # Show the data tree view window
+    # f1.show()
+
+    # Get equilibrium data and set plugin frame
+    app.frame = PlotFrame(dataDict, parent=f1.dataTreeView)
+
+    # Show frame
     app.frame.show()
+
+    # Keep the application running
     sys.exit(app.exec_())
+
+
---
+++ imasviz/VizPlugins/viz_equi/test_equilibriumcharts_plugin.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+"""This example demonstrates the procedure of directly running equilibrium
+plugin for specified database using script.
+Note: Script is adjusted to use IMAS database found on the GateWay.
+"""
+
+# A module providing a number of functions and variables that can be used to
+# manipulate different parts of the Python runtime environment.
+import sys
+# PyQt library imports
+from PyQt5.QtWidgets import QApplication
+# IMASViz source imports
+from imasviz.Viz_API import Viz_API
+from imasviz.VizDataSource.QVizDataSourceFactory import \
+    QVizDataSourceFactory
+from imasviz.VizUtils.QVizGlobalOperations import QVizGlobalOperations
+from imasviz.VizPlugins.viz_equi import equilibriumcharts
+
+# Set object managing the PyQt GUI application's control flow and main
+# settings
+app = QApplication(sys.argv)
+
+# Check if necessary system variables are set
+QVizGlobalOperations.checkEnvSettings()
+
+# Set Application Program Interface
+api = Viz_API()
+
+# Set data source retriever/factory
+dataSourceFactory = QVizDataSourceFactory()
+
+# Load IMAS database and build the data tree view frame
+f1 = api.CreateDataTree(dataSourceFactory.create(shotNumber=52344,
+                                                 runNumber=0,
+                                                 userName='g2penkod',
+                                                 imasDbName='viztest'))
+
+# Get equilibrium treeWidget item (QVizTreeNode)
+eq_item = f1.dataTreeView.IDSRoots['equilibrium']
+
+# Get selected item/subject data dict
+dataDict = eq_item.getDataDict()
+
+# Show the data tree view window
+# f1.show()
+
+# Get equilibrium data and set plugin frame
+app.frame = equilibriumcharts.PlotFrame(dataDict, parent=f1.dataTreeView)
+
+# Show frame
+app.frame.show()
+
+# Keep the application running
+sys.exit(app.exec_())
