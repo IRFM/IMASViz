fd513abdde8f4a6bdba18e19e2901ac8750e3587
Dejan Penko <dejan.penko@lecad.fs.uni-lj.si>
2019-02-18 16:19:21 +0100

    Set pyqt slots priperly in order to be able to use them in the QT designer.

--- imasviz/VizPlugins/viz_solps/plotEPGGD.py
+++ imasviz/VizPlugins/viz_solps/plotEPGGD.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python3
 
 import sys
+import os
 from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QAction, \
                             QVBoxLayout, QHBoxLayout, QLabel, QLineEdit, \
                             QSpacerItem, QSizePolicy, QPushButton
@@ -22,41 +23,83 @@ import numpy as np
 
 from getEPGGD import getEPGGD
 
-import random
+class GetVars:
+    names = ['SHOT', 'RUN', 'USER', 'DEVICE', 'VERSION']
+    numOfParams = len(names)
+    shot, run, user, device, version = range(numOfParams)
+
+    defaultValues = {}
+    defaultValues[shot] = '122264'
+    defaultValues[run] = '1'
+    defaultValues[user] = os.getenv('USER')
+    defaultValues[device] = 'iter'
+    defaultValues[version] = '3'
 
 class plotEPGGD(QWidget):
     """Plot edge_profiles (EP) IDS GGD.
     """
 
-    def __init__(self, ids, parent=None, *args, **kwargs):
+    def __init__(self, ids=None, parent=None, *args, **kwargs):
         QWidget.__init__(self, parent)
+
+        self.checkDisplay()
+
         self.ids = ids
         self.setLayout(QVBoxLayout())
-        self.canvas = PlotCanvas(self, width=10, height=8)
+        self.canvas = PlotCanvas(self, width=5, height=4)
         self.toolbar = NavigationToolbar(self.canvas, self)
-        self.layout().addWidget(self.toolbar)
         self.layout().addWidget(self.canvas)
+        self.layout().addWidget(self.toolbar)
+        self.resize(200,200)
 
-        # TODO: setShot, setRun, setUser, setDevice
+    @pyqtSlot()
+    def checkIDS(self):
+        self.vars = {}
+        # If IDS object is not provided, display dialog window where the IDS
+        # parameters can be specified. Then open the specified IDS
+        if self.ids != None:
+            return
+        else:
+            from getIDS import GetIDSWrapper, GetDialog
+            for i in range(GetVars.numOfParams):
+                # At the begining clear all parameters
+                self.vars[i] = ''
 
+            dialog = GetDialog(self)
+            dialog.prepareWidgets(self.vars)
+            if dialog.exec_():
+                self.vars = dialog.on_close()
+
+            self.ids = GetIDSWrapper(self.vars).getIDS()
 
     @pyqtSlot()
     def plotData(self):
+        """Populate (plot) the canvas.
+        """
         self.canvas.plotData()
+
+    @pyqtSlot( )
+    def checkDisplay(self):
+        try:
+            os.environ['DISPLAY']
+        except:
+            logging.error('No display available!')
 
 class PlotCanvas(FigureCanvas):
     def __init__(self, parent=None, width=10, height=8, dpi=100):
-        self.ids = parent.ids
 
+        self.parent = parent
         self.figure = Figure(figsize=(width, height), dpi=dpi)
         FigureCanvas.__init__(self, self.figure)
         self.setParent(parent)
         FigureCanvas.setSizePolicy(self, QSizePolicy.Expanding, QSizePolicy.Expanding)
         FigureCanvas.updateGeometry(self)
 
+    @pyqtSlot()
     def plotData(self):
         """Plots edge data to 2D VTK.
         """
+        self.ids = self.parent.ids
 
         logging.info('Getting IDS')
         self.ids.edge_profiles.get()
@@ -133,7 +176,8 @@ class PlotCanvas(FigureCanvas):
         self.draw()
 
 if __name__ == '__main__':
-    from getIDS import GetIDSWrapper
+    from getIDS import GetIDSWrapper, GetDialog, GetVars
+    import getopt
 
     root = logging.getLogger()
     root.setLevel(logging.DEBUG)
@@ -141,15 +185,84 @@ if __name__ == '__main__':
     ch.setLevel(logging.DEBUG)
     root.addHandler(ch)
 
-
     app = QApplication(sys.argv)
 
-    Vars = {0: 122264, 1: 1, 2: 'penkod', 3: 'iter', 4: '3'}
-    ids = GetIDSWrapper(Vars).getIDS()
-    plotWidget = plotEPGGD(ids)
+    mainWindow = QMainWindow()
+
+    # If any IDS parameters are given as an argument (via terminal), open the
+    # specified IDS. Otherwise, open dialog where the parameters can be set
+    if len(sys.argv) > 1:
+        # Set IDS object, open it and provide it as an argument to plotEPGGD
+        # Vars = {0: 122264, 1: 1, 2: 'penkod', 3: 'iter', 4: '3'}
+
+        # For launching python script directly from terminal with python command
+        Vars = {}
+        Help = """
+                This is used for testing the plotting data from an edge_profilesIDS.
+
+                In order to run this plugin, shot, run, user, device and version must
+                be defined. Example (terminal):
+
+                python3 solpswidget.py --shot=122264 --run=1 --user=penkod \
+                --device=iter --version=3
+                
+                """
+        try:
+            opts, args = getopt.getopt(sys.argv[1:], "srudvh", ["shot=",
+                                                                "run=",
+                                                                "user=",
+                                                                "device=",
+                                                                "version=",
+                                                                "help"])
+
+            for opt, arg in opts:
+                # print opt, arg
+                if opt in ("-s", "--shot"):
+                    Vars[GetVars.shot] = int(arg)
+                elif opt in ("-r", "--run"):
+                    Vars[GetVars.run] = int(arg)
+                elif opt in ("-u", "--user"):
+                    Vars[GetVars.user] = arg
+                elif opt in ("-t", "--device"):
+                    Vars[GetVars.device] = arg
+                elif opt in ("-v", "--version"):
+                    Vars[GetVars.version] = arg
+                if opt in ("-h", "--help"):
+                    print(Help % (os.environ['USER'], os.path.expanduser('~')))
+                    sys.exit()
+
+            ids = GetIDSWrapper(Vars).getIDS()
+            plotWidget = plotEPGGD(ids)
+
+        except Exception:
+            print('Supplied option not recognized!')
+            print('For help: -h / --help')
+            # sys.exit(2)
+            print('Switching to dialog window')
+            plotWidget = plotEPGGD()
+
+
+        if len(Vars) < GetVars.numOfParams:
+            print('Not enough variables defined!')
+            print('For help: -h / --help')
+            # sys.exit(2)
+            print('Switching to dialog window')
+            plotWidget = plotEPGGD()
+        elif len(Vars) > GetVars.numOfParams:
+            print('Too many variables defined!')
+            print('For help: -h / --help')
+            # sys.exit(2)
+            print('Switching to dialog window')
+            plotWidget = plotEPGGD()
+
+    else:
+        # Open IDS (specify parameters using dialog)
+        plotWidget = plotEPGGD()
+
+    plotWidget.checkIDS()
+
     plotWidget.plotData()
 
-    mainWindow = QMainWindow()
     title = 'Test: Plot edge_profiles GGD'
 
     mainWindow.setWindowTitle(title)
