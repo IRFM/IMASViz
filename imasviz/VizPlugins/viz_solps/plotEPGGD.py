059075f47a88dbcbdc5a214dd1d0bfcd3e46ce3a
Dejan Penko <dejan.penko@lecad.fs.uni-lj.si>
2019-02-20 17:09:36 +0100

    First try of implementing dialog for setting the GGD slice, grid_ggd slice, grid subset and quantity, determining the data which should be passed to plot.

--- imasviz/VizPlugins/viz_solps/plotEPGGD.py
+++ imasviz/VizPlugins/viz_solps/plotEPGGD.py
@@ -4,8 +4,9 @@ import sys
 import os
 from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QAction, \
                             QVBoxLayout, QHBoxLayout, QLabel, QLineEdit, \
-                            QSpacerItem, QSizePolicy, QPushButton
-from PyQt5.QtGui import QIcon
+                            QSpacerItem, QSizePolicy, QPushButton, QDialog, \
+                            QFormLayout, QDialogButtonBox, QComboBox
+from PyQt5.QtGui import QIntValidator
 from PyQt5.QtCore import QSize, pyqtSlot
 
 
@@ -23,7 +24,7 @@ import numpy as np
 
 from getEPGGD import getEPGGD
 
-class GetVars:
+class GetIDSVars:
     names = ['SHOT', 'RUN', 'USER', 'DEVICE', 'VERSION']
     numOfParams = len(names)
     shot, run, user, device, version = range(numOfParams)
@@ -35,16 +36,48 @@ class GetVars:
     defaultValues[device] = 'iter'
     defaultValues[version] = '3'
 
+class GetGGDVars:
+    names = ['grid_ggd_slice', 'ggd_slice']
+    numOfParams = len(names)
+    grid_ggd_slice, ggd_slice = range(numOfParams)
+
+    defaultValues = {}
+    defaultValues[grid_ggd_slice] = '0'
+    defaultValues[ggd_slice] = '0'
+
 class plotEPGGD(QWidget):
+
     """Plot edge_profiles (EP) IDS GGD.
     """
-
     def __init__(self, parent=None, ids=None, *args, **kwargs):
         QWidget.__init__(self, parent)
 
+        # Check if display is available (display is mandatory, as this is
+        # PyQt5 widget)
         self.checkDisplay()
+        # Set parent
         self.parent = parent
 
+        self.grid_ggd_slice = 0
+        self.ggd_slice = 0
+        self.gridSubset_id = 0
+        self.gridSubsetDict = {}
+        self.num_gridSubsets = 0
+        self.num_ggd_slices = 0
+        self.num_grid_ggd_slices = 0
+
+        self.gs_id = 0
+        self.qLabel = ''
+        self.qValues = 0
+
+        self.quantityDict = {}
+
+        self.ggdVars = {}
+        for i in range(GetGGDVars.numOfParams):
+            # At the begining clear all parameters
+            self.ggdVars[i] = ''
+
+        # Check if the widget is run from IMASViz or as standalone
         try:
             # Check if the correct DTV (from IMASViz) is available
             self.dataTreeView = self.parent.parent.parent
@@ -54,14 +87,26 @@ class plotEPGGD(QWidget):
             self.usingIMASViz = False
             self.dataTreeView = None
 
+        # Set IDS object
         self.ids = ids
-
+        # Set layout
         self.setLayout(QVBoxLayout())
+        # Set empty matplotliv canvas
         self.canvas = PlotCanvas(self, width=5, height=4)
+        # Set matplotlib toolbar
         self.toolbar = NavigationToolbar(self.canvas, self)
+        # Add widgets to layout
         self.layout().addWidget(self.canvas)
         self.layout().addWidget(self.toolbar)
+        # Resize widget
         self.resize(200,200)
+
+    @pyqtSlot( )
+    def checkDisplay(self):
+        try:
+            os.environ['DISPLAY']
+        except:
+            logging.error('No display available!')
 
     @pyqtSlot()
     def checkIDS(self):
@@ -82,8 +127,8 @@ class plotEPGGD(QWidget):
             self.getIDSfromIMASViz()
         else:
             from getIDS import GetIDSWrapper, GetDialog
-            for i in range(GetVars.numOfParams):
-                # At the begining clear all parameters
+            for i in range(GetIDSVars.numOfParams):
+                # At the beginning clear all parameters
                 self.vars[i] = ''
 
             dialog = GetDialog(self)
@@ -92,6 +137,24 @@ class plotEPGGD(QWidget):
                 self.vars = dialog.on_close()
 
             self.ids = GetIDSWrapper(self.vars).getIDS()
+
+        logging.info('Getting IDS')
+        self.ids.edge_profiles.get()
+
+        # TODO: run it outside checkIDS
+        self.setGGD()
+
+    @pyqtSlot()
+    def setGGD(self):
+        dialog = GetGGDDialog(self)
+        dialog.prepareWidgets(self.ggdVars)
+        if dialog.exec_():
+            self.ggdVars, self.gs_id, self.qLabel, self.qValues = \
+                dialog.on_close()
+            return True
+        else:
+            # Canceled!
+            return False
 
     @pyqtSlot()
     def getIDSfromIMASViz(self):
@@ -128,17 +191,26 @@ class plotEPGGD(QWidget):
             self.ids.edge_profiles.get()
 
     @pyqtSlot()
+    def getQuantityValues(self):
+        return self.qValues
+
+    @pyqtSlot()
+    def getQuantityLabel(self):
+        return self.qLabel
+
+    @pyqtSlot()
+    def getGridSubsetID(self):
+        return self.gs_id
+
+    @pyqtSlot()
+    def getGGDVars(self):
+        return self.ggdVars
+
+    @pyqtSlot()
     def plotData(self):
         """Populate (plot) the canvas.
         """
         self.canvas.plotData()
-
-    @pyqtSlot( )
-    def checkDisplay(self):
-        try:
-            os.environ['DISPLAY']
-        except:
-            logging.error('No display available!')
 
 class PlotCanvas(FigureCanvas):
     def __init__(self, parent=None, width=10, height=8, dpi=100):
@@ -154,10 +226,18 @@ class PlotCanvas(FigureCanvas):
     def plotData(self):
         """Plots edge data to 2D VTK.
         """
-        self.ids = self.parent.ids
 
-        logging.info('Getting IDS')
-        self.ids.edge_profiles.get()
+        ggdVars = self.parent.getGGDVars()
+        # TODO: Contains all possible arrays and values. Set that only the
+        # one required is passed
+        qValues = self.parent.getQuantityValues()
+        qLabel = self.parent.getQuantityLabel()
+        gs_id = self.parent.getGridSubsetID() -1
+
+        gg = ggdVars[0] # grid_ggd index
+        g = ggdVars[1]  # ggd index
+
+        self.ids = self.parent.ids
         self.ep = self.ids.edge_profiles
 
         getGGD = getEPGGD(self.ep)
@@ -166,34 +246,38 @@ class PlotCanvas(FigureCanvas):
 
         # Reading IDS grid geometry and physics quantities array
         nodes = np.zeros(shape=(self.num_obj_0D, 2))
-        quad_conn_array = np.zeros(shape=(self.num_obj_2D, 4), dtype=np.int)
-        # quad_values_array = np.zeros(self.num_obj_2D)
+        # quad_conn_array = np.zeros(shape=(self.num_obj_2D, 4), dtype=np.int)
+        nElements = len(self.ep.grid_ggd[gg].grid_subset[gs_id].element)
+        quad_conn_array = np.zeros(shape=(nElements, 4), dtype=np.int)
 
         # List of nodes and corresponding coordinates (2D spade - x and y)
         for i in range(self.num_obj_0D):
             # X coordinate
-            nodes[i][0] = self.ep.grid_ggd[0].space[0].objects_per_dimension[0].object[i].geometry[0]
+            nodes[i][0] = \
+                self.ep.grid_ggd[gg].space[0].objects_per_dimension[0].object[i].geometry[0]
             # Y coordinate
-            nodes[i][1] = self.ep.grid_ggd[0].space[0].objects_per_dimension[0].object[i].geometry[1]
-
-        # Connectivity array. Each quad is formed using 4 nodes/points
-        for i in range(self.num_obj_2D):
-            for j in range(0,4):
-                quad_conn_array[i][j] = self.ep.grid_ggd[0].space[0].objects_per_dimension[2].object[i].nodes[j] - 1
-
-        # print("quad_conn_array: ", quad_conn_array)
-
-        # Values corresponding to quads
-
-
+            nodes[i][1] = \
+                self.ep.grid_ggd[gg].space[0].objects_per_dimension[0].object[i].geometry[1]
+        #
+        # # Connectivity array. Each quad is formed using 4 nodes/points
         # for i in range(self.num_obj_2D):
-        #     quad_values_array[i] = i
+        #     for j in range(0,4):
+        #         quad_conn_array[i][j] = \
+        #             self.ep.grid_ggd[gg].space[0].objects_per_dimension[2].object[i].nodes[j] - 1
 
-        # TODO: check ... electrons.density[0].grid_subset_index etc.
+        for i in range(nElements):
+            object = self.ep.grid_ggd[gg].grid_subset[gs_id].element[
+                i].object[0]
+            ind = object.index - 1
+            s = object.space - 1
+            d = object.dimension - 1
 
-        quad_values_array = self.ep.ggd[0].electrons.temperature[0].values
+            for j in range(0,4):
+                quad_conn_array[i][j] = \
+                    self.ep.grid_ggd[gg].space[s].objects_per_dimension[
+                        d].object[ind].nodes[j] - 1
 
-        self.showMeshPlot(nodes, quad_conn_array, quad_values_array)
+        self.showMeshPlot(nodes, quad_conn_array, qValues)
 
     def showMeshPlot(self, nodes, elements, values):
 
@@ -230,8 +314,159 @@ class PlotCanvas(FigureCanvas):
 
         self.draw()
 
+class GetGGDDialog(QDialog):
+    """Dialog Demanding the grid_ggd and ggd slice together with grid
+    subset and quantity.
+    """
+
+    def __init__(self, parent=None):
+        super(GetGGDDialog, self).__init__(parent)
+
+        self.ids = parent.ids
+        self.gridSubsetDict = {}
+        self.quantityDict = {}
+
+    def prepareWidgets(self, parameters, title='GGD Variables',):
+
+        self.setModal(True)
+
+        self.setWindowTitle(title)
+
+        formLayout = QFormLayout(self)
+
+        self.lineEditContainer = {}
+
+        for i in range(GetGGDVars.numOfParams):
+            currLineEdit = QLineEdit()
+            currLineEdit.setText(GetGGDVars.names[i])
+            currLineEdit.setObjectName(GetGGDVars.names[i])
+            self.lineEditContainer[i] = currLineEdit
+            if parameters[i]:
+                currLineEdit.setText(parameters[i])
+            else:
+                currLineEdit.setText(GetGGDVars.defaultValues[i])
+
+            formLayout.addRow(GetGGDVars.names[i], currLineEdit)
+
+        # Setting integer validator for run and shot numbers.
+        self.lineEditContainer[GetGGDVars.ggd_slice].setValidator(
+            QIntValidator())
+        self.lineEditContainer[GetGGDVars.grid_ggd_slice].setValidator(QIntValidator())
+
+        # Set comboboxes
+        self.combobox_gridSubset = QComboBox()
+        self.combobox_quantity = QComboBox()
+
+        self.gg_lineEdit = self.findChild(QLineEdit, 'grid_ggd_slice')
+        self.g_lineEdit = self.findChild(QLineEdit, 'ggd_slice')
+
+        self.populateComboBoxGS()
+        self.populateComboBoxQ()
+
+        self.gg_lineEdit.textChanged.connect(self.populateComboBoxGS)
+
+        self.combobox_gridSubset.currentTextChanged.connect(
+            self.populateComboBoxQ)
+
+        self.findChild(QLineEdit, 'grid_ggd_slice').text()
+        self.findChild(QLineEdit, 'ggd_slice')
+
+        formLayout.addRow('Grid Subset', self.combobox_gridSubset)
+        formLayout.addRow('Grid Subset Quantity', self.combobox_quantity)
+
+        # Adding the Ok and Cancel button.
+        dialog_button_box = QDialogButtonBox()
+        dialog_button_box.setStandardButtons(QDialogButtonBox.Ok |
+                                             QDialogButtonBox.Cancel)
+        dialog_button_box.accepted.connect(self.accept)
+        dialog_button_box.rejected.connect(self.reject)
+        formLayout.addRow(dialog_button_box)
+
+    def populateComboBoxGS(self):
+        """Populate grid subset combobox with choices
+        corresponding to set grid_ggd and ggd indices.
+        """
+
+        self.combobox_gridSubset.clear()
+
+        if self.gg_lineEdit.text().isdigit() != True:
+            return
+
+        gg = int(self.gg_lineEdit.text())
+
+
+        for i in range(len(self.ids.edge_profiles.grid_ggd[gg].grid_subset)):
+            gs_obj = self.ids.edge_profiles.grid_ggd[gg].grid_subset[i]
+            # Only 2D grid subsets supported for now (first object dimension
+            # parameter = 3 (fortan notation, 2D -> 2+1 = 3)
+            if gs_obj.element[0].object[0].dimension == 3:
+                gs_name = gs_obj.identifier.name
+                self.combobox_gridSubset.addItem(gs_name)
+                self.gridSubsetDict[gs_name] = i + 1 # Fortran notation in IDS
+
+    def populateComboBoxQ(self):
+        """Populate grid subset combobox with choices
+        corresponding to set grid_ggd and ggd indices and grid subset.
+        """
+
+        self.combobox_quantity.clear()
+
+        if self.gg_lineEdit.text().isdigit() != True:
+            return
+
+        if self.g_lineEdit.text().isdigit() != True:
+            return
+
+        gg = int(self.gg_lineEdit.text())
+        g = int(self.g_lineEdit.text())
+        gs_name = self.combobox_gridSubset.currentText()
+        gs_id = self.gridSubsetDict[gs_name]
+
+        ggd = self.ids.edge_profiles.ggd[g]
+
+        # Electron temperature
+        for i in range(len(ggd.electrons.temperature)):
+            if ggd.electrons.temperature[i].grid_subset_index == gs_id:
+                qLabel = 'Electron Temperature'
+                self.combobox_quantity.addItem(qLabel)
+                self.quantityDict[qLabel] = ggd.electrons.temperature[i].values
+
+        # Electron density
+        for i in range(len(ggd.electrons.density)):
+            if ggd.electrons.density[i].grid_subset_index == gs_id:
+                qLabel = 'Electron Density'
+                self.combobox_quantity.addItem(qLabel)
+                self.quantityDict[qLabel] = ggd.electrons.density[i].values
+
+    def getValue(self, Id):
+        return self.lineEditContainer[Id].text()
+
+    def on_close(self):
+        # Returning a dictionary of values. The values are defined in
+        # enumerator class GetGGDVars.
+
+        variables = {}
+
+        for i in range(GetGGDVars.numOfParams):
+            variables[i] = self.getValue(i)
+
+        # Checking if validating Integers.
+        try:
+            variables[GetGGDVars.grid_ggd_slice] = int(variables[GetGGDVars.grid_ggd_slice])
+            variables[GetGGDVars.ggd_slice] = \
+                int(variables[GetGGDVars.ggd_slice])
+        except ValueError as e:
+            variables[GetGGDVars.grid_ggd_slice] = -1
+            variables[GetGGDVars.ggd_slice] = -1
+
+        curr_quantity = self.combobox_quantity.currentText()
+        curr_gridSubset_id = int(self.gridSubsetDict[
+            self.combobox_gridSubset.currentText()])
+
+        return variables, curr_gridSubset_id, curr_quantity, \
+               self.quantityDict[curr_quantity]
+
 if __name__ == '__main__':
-    from getIDS import GetIDSWrapper, GetDialog, GetVars
     import getopt
 
     root = logging.getLogger()
@@ -273,15 +508,15 @@ if __name__ == '__main__':
             for opt, arg in opts:
                 # print opt, arg
                 if opt in ("-s", "--shot"):
-                    Vars[GetVars.shot] = int(arg)
+                    Vars[GetIDSVars.shot] = int(arg)
                 elif opt in ("-r", "--run"):
-                    Vars[GetVars.run] = int(arg)
+                    Vars[GetIDSVars.run] = int(arg)
                 elif opt in ("-u", "--user"):
-                    Vars[GetVars.user] = arg
+                    Vars[GetIDSVars.user] = arg
                 elif opt in ("-t", "--device"):
-                    Vars[GetVars.device] = arg
+                    Vars[GetIDSVars.device] = arg
                 elif opt in ("-v", "--version"):
-                    Vars[GetVars.version] = arg
+                    Vars[GetIDSVars.version] = arg
                 if opt in ("-h", "--help"):
                     print(Help % (os.environ['USER'], os.path.expanduser('~')))
                     sys.exit()
@@ -297,13 +532,13 @@ if __name__ == '__main__':
             plotWidget = plotEPGGD()
 
 
-        if len(Vars) < GetVars.numOfParams:
+        if len(Vars) < GetIDSVars.numOfParams:
             print('Not enough variables defined!')
             print('For help: -h / --help')
             # sys.exit(2)
             print('Switching to dialog window')
             plotWidget = plotEPGGD()
-        elif len(Vars) > GetVars.numOfParams:
+        elif len(Vars) > GetIDSVars.numOfParams:
             print('Too many variables defined!')
             print('For help: -h / --help')
             # sys.exit(2)
@@ -315,6 +550,7 @@ if __name__ == '__main__':
         plotWidget = plotEPGGD()
 
     plotWidget.checkIDS()
+    plotWidget.setGGD()
 
     plotWidget.plotData()
 
