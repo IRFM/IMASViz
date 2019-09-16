ae0a54a853371ba69559020bdce1a4b2f43387f0
penkod <dejan.penko@lecad.fs.uni-lj.si>
2019-09-16 11:17:29 +0200

    Bugfix when time index gets wrongly set as array index of profiles_1d[:].

--- imasviz/VizDataAccess/QVizIMASNativeDataAccess.py
+++ imasviz/VizDataAccess/QVizIMASNativeDataAccess.py
@@ -32,7 +32,12 @@ class QVizIMASNativeDataAccess:
 
             r = np.array([rval])
 
-            coordinate1 = treeNode.evaluateCoordinate1At(itimeValue)
+            # Note: patch added while experiencing issues in radiation.process[i].profiles_1d[j]
+            if "profiles_1d[j]" in treeNode.infoDict['aos']:
+                coordinate1 = treeNode.evaluateCoordinate1At(int(treeNode.infoDict['j']))
+            else:
+                coordinate1 = treeNode.evaluateCoordinate1At(itimeValue)
+            print("*coordinate1: ", coordinate1)
 
             if  treeNode.isCoordinate1_time_dependent(): #coordinate1 is a function of time
                 t = QVizGlobalOperations.getCoordinate1D_array(ids, treeNode.getNodeData(), coordinate1)
