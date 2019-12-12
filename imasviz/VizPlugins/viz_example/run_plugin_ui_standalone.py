a1800c667600bd89a852c3ed38793a4272092382
penkod <dejan.penko@lecad.fs.uni-lj.si>
2019-12-12 08:58:34 +0100

    Add scripts to run the SOLPS and example plugin in a standalone way (using the .ui file).

---
+++ imasviz/VizPlugins/viz_example/run_plugin_ui_standalone.py
@@ -0,0 +1,11 @@
+# An example running the UI plugin in standalone way.
+
+from PyQt5.QtWidgets import QApplication
+from PyQt5 import uic
+import sys
+
+app = QApplication(sys.argv)
+ui_obj = uic.loadUi("examplePlugin.ui")
+ui_obj.show()
+
+sys.exit(app.exec_())
