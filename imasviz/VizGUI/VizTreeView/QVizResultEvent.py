e0c38a68151dc03ed5b35796d1093b177e187b50
wasikj <jwasik@man.poznan.pl>
2022-11-30 08:44:14 +0100

    IMAS-4325: fix QEvent.Type error

--- imasviz/VizGUI/VizTreeView/QVizResultEvent.py
+++ imasviz/VizGUI/VizTreeView/QVizResultEvent.py
@@ -12,5 +12,5 @@ class QVizResultEvent(QEvent):
             eventID (int) : Custom even ID.
         """
         #thread-safe
-        super(QVizResultEvent, self).__init__(eventID)
+        super(QVizResultEvent, self).__init__(QEvent.Type(eventID))
         self.data = idsData
